1bf096538296cbba7e5e55905a36a20d13cb88f6
ginedou0315 <ginelle.doubek@gmail.com>
2024-12-16 15:25:15 +0100

    update JS  and added moment

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,23 @@
+function updateTime() {
+  let zurichElement = document.querySelector("#zurich");
+  let zurichDateElement = zurichElement.querySelector(".date");
+  let zurichTimeELement = zurichElement.querySelector(".time");
+  let zurichTime = moment().tz("Europe/Zurich");
 
+  zurichDateElement.innerHTML = zurichTime.format("MMMM Do YYYY");
+  zurichTimeELement.innerHTML = zurichTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let manilaElement = document.querySelector("#manila");
+  let manilaDateElement = manilaElement.querySelector(".date");
+  let manilaTimeELement = manilaElement.querySelector(".time");
+  let manilaTime = moment().tz("Asia/Manila");
+
+  manilaDateElement.innerHTML = manilaTime.format("MMMM Do YYYY");
+  manilaTimeELement.innerHTML = manilaTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
